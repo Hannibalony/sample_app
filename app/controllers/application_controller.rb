0931aabeb2a6e0bfc85cff43379fd39a87373e48
Hannibalony <justinecantado@gmail.com>
2017-05-19 00:35:19 +0000

    Hmmm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   
-  def test
-    render html: "Text"
-  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,41 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  
+  def setup
+    @base_title = "Ruby on Rails"
+  end
+  
+  test "should get root" do
+    get '/'
     assert_response :success
+  end
+  
+
+  
+  test "should get home" do
+    get static_pages_home_url     #get request for home url
+    assert_response :success      #Make sure the request returns response 200
+    assert_select 'title', "Home | #{@base_title}" #Make sure the title of home page is select as 'Home'
   end
 
   test "should get help" do
     get static_pages_help_url
-    assert_response :success
+    assert_response :success      
+    assert_select 'title', "Help | #{@base_title}" #Make sure the title of help page is select as 'Help'
   end
 
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select 'title', "About | #{@base_title}" #Make sure the title of about page is select as 'About'
+  end
+  
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
+  end
+  
 end
